a9ce9b09b3534379697d4055aaae4bf54c81ee90
Ingy döt Net <ingy@ingy.net>
2018-06-05 00:56:00 -0700

    Get python unittest tests passing

--- testml/run/unit.py
+++ testml/run/unit.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import re
+
 from testml.util import *
 
 import unittest
@@ -14,21 +16,33 @@ class TestMLRunUnit(TestMLRun):
   def __init__(self, **params):
     TestMLRun.__init__(self, **params)
 
-  def test_begin(self):
+  def testml_begin(self):
+    print 'Testing:', self.file
     self.suite = unittest.TestSuite()
 
-  def test_end(self):
+  def testml_end(self):
     verbosity = 1
     runner = unittest.TextTestRunner(verbosity=verbosity)
     runner.resultclass = TestResult
     runner.run(self.suite)
+    print
 
-  def test_eq(self, got, want, label):
-    self.suite.addTest(testml_eq(got, want, label))
+  def testml_eq(self, got, want, label):
+    self.suite.addTest(testml_case_eq(got, want, label))
 
-class testml_eq(unittest.TestCase):
+  def testml_like(self, got, want, label):
+    self.suite.addTest(testml_case_like(got, want, label))
+
+  def testml_has(self, got, want, label):
+    self.suite.addTest(testml_case_has(got, want, label))
+
+  def testml_list_has(self, got, want, label):
+    self.suite.addTest(testml_case_has(got, want, label))
+
+
+class testml_case_eq(unittest.TestCase):
   def __init__(self, got, want, label):
-    super(testml_eq, self).__init__()
+    super(testml_case_eq, self).__init__()
 
     self.got = got
     self.want = want
@@ -36,6 +50,31 @@ class testml_eq(unittest.TestCase):
 
   def runTest(self):
     self.assertEqual(self.got, self.want, self.label)
+
+
+class testml_case_like(unittest.TestCase):
+  def __init__(self, got, want, label):
+    super(testml_case_like, self).__init__()
+
+    self.got = got
+    self.want = want
+    self.label = label
+
+  def runTest(self):
+    self.assertTrue(re.search(self.want, self.got), self.label)
+
+
+class testml_case_has(unittest.TestCase):
+  def __init__(self, got, want, label):
+    super(testml_case_has, self).__init__()
+
+    self.got = got
+    self.want = want
+    self.label = label
+
+  def runTest(self):
+    self.assertTrue(self.want in self.got, self.label)
+
 
 # See: https://www.pythonsheets.com/notes/python-tests.html#customize-test-report
 class TestResult(unittest.TextTestResult):
